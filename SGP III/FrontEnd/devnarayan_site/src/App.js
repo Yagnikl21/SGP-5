545b828da2b29869ed8874264f4dcae9a3568090
Yagnik Ladani <y.m.ladnai972@gmail.com>
2023-07-22 15:24:29 +0530

    Add product section and fix routing problem

--- SGP III/FrontEnd/devnarayan_site/src/App.js
+++ SGP III/FrontEnd/devnarayan_site/src/App.js
@@ -1,15 +1,14 @@
 import React from 'react'
 import Home from './Page/Home/Home'
-import { Route, Router, browserrouter , router , routes } from 'react-router-dom'
+import { Route, Routes } from 'react-router-dom'
 import About from './Page/About/About'
 
 function App() {
   return (
-    <Router>
-      <Route path='/' Component={<Home />} />
-      <Route path='/about' Component={<About />} />
-      
-    </Router>
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/about' element={<About />} />
+    </Routes>
   )
 }
 
