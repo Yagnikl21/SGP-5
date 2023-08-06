42ba179c12ac45ee44768d761d333126108539f1
Yagnik Ladani <y.m.ladnai972@gmail.com>
2023-08-06 14:51:50 +0530

    Add product page

--- SGP III/backend/src/app.js
+++ SGP III/backend/src/app.js
@@ -4,6 +4,7 @@ const express=require("express")
 require("dotenv").config();
 const mongoose=require("mongoose")
 const app=express()
+const cors = require("cors");
 const multer = require("multer");
 const icecreamroute=require("./routes/icecream")
 const userroute=require("./routes/user")
@@ -27,6 +28,27 @@ app.use("/",userroute);
 app.use("/icecream/",icecreamroute)
 
 
+<<<<<<< HEAD
+=======
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then((responce) => {
+    console.log("Connected to MongoDB , ", responce.connection.name);
+  })
+  .catch((err) => console.log(err));
+  app.use(
+    cors({
+      // origin: "http://localhost:3000"
+      // methods: "GET,POST,PUT,DELETE",
+    })
+  );
+  
+  app.use("/",userroute);
+  app.use("/icecream/",icecreamroute)
+>>>>>>> 754e6d0a2c580992815c1bbbde90979720ac873a
 const port = process.env.PORT || 8080;
 app.listen(port,()=>console.log("Backend is running on port",port))
 
