ae39ac77fa776a851aaa5d57705fb174da55a609
Yagnik Ladani <y.m.ladnai972@gmail.com>
2023-08-26 17:36:12 +0530

    Create Order Page and Route

--- SGP III/FrontEnd/devnarayan_site/src/App.js
+++ SGP III/FrontEnd/devnarayan_site/src/App.js
@@ -5,7 +5,7 @@ import Signup from "./Page/Login/Signup";
 import { Route, Routes } from "react-router-dom";
 import About from "./Page/About/About";
 import Forgot from "./Page/Login/Forgot";
-
+import Order from './Page/Order/order'
 import Products from "./Page/Products/Products";
 
 function App() {
@@ -18,6 +18,7 @@ function App() {
       <Route path="/login" element={<Login />} />
       <Route path="/signup" element={<Signup />} />
       <Route path="/Forgot" element={<Forgot />} />
+      <Route path="/orders" element={<Order/>} />
     </Routes>
   );
 }
---
+++ SGP III/FrontEnd/devnarayan_site/src/Page/Order/order.jsx
@@ -0,0 +1,98 @@
+import React from 'react'
+import './order.scss'
+import test from '../../assets/Images/product-1.jpg'
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
+
+export default function order() {
+
+    const array = [1, 2, 3, 4];
+    let count = 0;
+    const showProduct = array.map((m) => {
+        count += 1;
+        return (
+            <>
+                {count > 1 && <hr />}
+                <div className="ord-product">
+                    <img src={test} alt="Ice-ceream" />
+                    <div className="product-det">
+                        <div className="ord-product-details">
+                            <h4>Vanilla Ice-cream</h4>
+                            <p>Description and bhahaha</p>
+                        </div>
+                        <span className="price">
+                            <h5>Price</h5>
+                            <h5>Quntity</h5>
+                            <h5>Total Price</h5>
+                        </span>
+                    </div>
+                </div>
+            </>
+        )
+    })
+
+    return (
+        <>
+            <div className="order">
+                <h1>Order Details</h1>
+                <div className="details">
+                    <div className="productList">
+                        <h3>Customer's Cart</h3>
+                        <br />
+                        {showProduct}
+                    </div>
+                    <div className="usedetails">
+                        <h3>Customer</h3>
+                        <br />
+                        <h5>UseName</h5>
+                        <h5>Email</h5>
+                        <h5>+919724460156</h5>
+                        <br />
+                        <hr />
+                        <br />
+                        <h4> Address </h4>
+                        <br />
+                        <FormControl fullWidth>
+                            <InputLabel id="demo-simple-select-label">Address</InputLabel>
+                            <Select
+                                labelId="demo-simple-select-label"
+                                id="demo-simple-select"
+                                // value={age}
+                                label="Age"
+                            // onChange={handleChange}
+                            >
+                                <MenuItem value={"newShreedep"}>NEW SHREEDEP</MenuItem>
+                                <MenuItem value={"nisrag"}>NISRAG</MenuItem>
+                                <MenuItem value={"prince"}>PRINCE</MenuItem>
+                                <MenuItem value={"darshan"}>DARSHAN</MenuItem>
+                                <MenuItem value={"om"}>OM</MenuItem>
+                                <MenuItem value={"girls'sHostel"}>GIRL'S HOSTEL(CHARUSAT CAMPUS)</MenuItem>
+                            </Select>
+                        </FormControl>
+                        <br />
+                        <br />
+                        <hr />
+                        <br />
+                        <h4>Summary</h4>
+                        <br />
+                        <span className='d-flex justify-content-between'>
+                            <p>Subtotal</p>
+                            <p>500.00</p>
+                        </span>
+                        <span className='d-flex justify-content-between'>
+                            <p>Tax</p>
+                            <p>0.00</p>
+                        </span>
+                        <hr/>
+                        <span className='d-flex justify-content-between'>
+                            <p>Total</p>
+                            <p>500.00</p>
+                        </span>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
