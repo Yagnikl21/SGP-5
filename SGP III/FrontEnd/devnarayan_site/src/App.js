4b489d5f79a3b0520ea3775a7cd5277385a3ef47
kushkansagara <kush02032004@gmail.com>
2023-09-01 10:15:00 +0530

    Add change password page

--- SGP III/FrontEnd/devnarayan_site/src/App.js
+++ SGP III/FrontEnd/devnarayan_site/src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import Home from "./Page/Home/Home";
 import Login from "./Page/Login/Login";
 import Signup from "./Page/Login/Signup";
+import Change from "./Page/Login/Change";
 import { Route, Routes } from "react-router-dom";
 import About from "./Page/About/About";
 import Forgot from "./Page/Login/Forgot";
@@ -18,6 +19,7 @@ function App() {
       <Route path="/login" element={<Login />} />
       <Route path="/signup" element={<Signup />} />
       <Route path="/Forgot" element={<Forgot />} />
+      <Route path="/Change" element={<Change />} />
     </Routes>
   );
 }
--- SGP III/FrontEnd/devnarayan_site/src/Page/Login/Forgot.jsx
+++ SGP III/FrontEnd/devnarayan_site/src/Page/Login/Forgot.jsx
@@ -1,11 +1,29 @@
-// import React, { useEffect } from 'react';
+import React, { useState } from 'react';
 import './Forgot.scss';
 import Img from '../Login/logo.png';
-//import { Link } from 'react-router-dom';
+import OTPInput from "otp-input-react";
 
-// import Signup from '../Index/Signup';
+const OtpInputCard = ({ title, sendOTP, otpEnabled, ...rest }) => {
+    const [OTP, setOTP] = useState("");
+
+
+
+    if (!otpEnabled) {
+        return null; // Don't render the OTP input if otpEnabled is false
+    }
+
+    return (
+        <div style={{ padding: 12 }}>
+            <div style={{ marginBottom: 12 }}>{title}</div>
+            <OTPInput value={OTP} onChange={setOTP} {...rest} />
+            <button>Verify OTP</button>
+        </div>
+    );
+};
 
 export default function Forgot() {
+    const [otpEnabled, setOtpEnabled] = useState(false);
+
     return (
         <>
             <div className="Forgot-card-body">
@@ -16,7 +34,7 @@ export default function Forgot() {
                         </div>
                         <div className="Forgot-card-header">
                             <h1>Forgot Password</h1>
-                            <div>Enter Your Email and we'll send you link to reset your password.</div>
+                            <div>Enter Your Email and we'll send you a link to reset your password.</div>
                         </div>
                         <form className="Forgot-card-form">
                             <div className="form-item">
@@ -25,17 +43,33 @@ export default function Forgot() {
                                     type="text"
                                     placeholder="Enter Email"
                                     id="emailForm"
-                                    autofocus=""
+                                    autoFocus=""
                                     required=""
                                 />
                             </div>
-                            {/* <a href="#">I forgot my password!</a> */}
-                            <button type="submit">Submit</button>
+                            <button
+                                type="button"
+                                onClick={(e) => {
+                                    e.preventDefault();
+                                    // Disable OTP input before sending OTP
+                                    setOtpEnabled(true); // Enable OTP input
+                                    // Handle other form submission logic here
+                                }}
+                            >
+                                Send OTP
+                            </button>
                         </form>
+
+                        <OtpInputCard
+                            title="Enter OTP here"
+                            otpEnabled={otpEnabled} // Pass the otpEnabled state to the component
+                            OTPLength={4}
+                            otpType="number"
+                            disabled={false}
+                        />
                     </div>
                 </div>
             </div>
         </>
-
     );
 }
