213cad93fd394c33cf9a76540eeac6f1e1e33a4d
kushkansagara <kush02032004@gmail.com>
2023-09-01 10:41:11 +0530

    Login bug resolve

--- SGP III/FrontEnd/devnarayan_site/src/feature/User/userSlice.js
+++ SGP III/FrontEnd/devnarayan_site/src/feature/User/userSlice.js
@@ -1,49 +1,54 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import axios from "axios";
 
-export const loginUser = createAsyncThunk('user/loginUser',
-    async(usersCredential)=>{
-        const request = await axios.post('http://localhost:8080/login',usersCredential);
-        const response = await request.data;       
-        localStorage.setItem('user',JSON.stringify(response));
-        return response;
-    }
-)
+export const loginUser = createAsyncThunk(
+  "user/loginUser",
+  async (usersCredential) => {
+    const request = await axios.post(
+      "http://localhost:8080/login",
+      usersCredential
+    );
+    const response = await request.data;
+    localStorage.setItem("user", JSON.stringify(response));
+    return response;
+  }
+);
 
-const localUser = JSON.parse(localStorage.getItem('user'));
-console.log(localUser , "LocalUser is OK");
+const localUser = JSON.parse(localStorage.getItem("user"));
+// const localUser = {
+//   name: "yagnik android",
+// };
+console.log(localUser, "LocalUser is OK");
 const userSlice = createSlice({
-    name:'user',
-    initialState:{
-        loading:false,
-        user:localUser.users,
-        error:null
-
-    },
-    extraReducers:(builder)=>{
-        builder
-        .addCase(loginUser.pending,(state)=>{
-            state.loading = true;
-            state.user=null;
-            state.error=null;
-        })
-        .addCase(loginUser.fulfilled,(state,action)=>{
-            state.loading = false;
-            state.user = action.payload;
-            state.error = null;
-        })
-        .addCase(loginUser.rejected , (state,action)=>{
-            state.loading = false;
-            state.user = null;
-            console.log(action.error.message);
-            if(action.error.message === 'Request failed with status code 401'){
-                state.error = 'Access Denied! Invalid Credentials';
-            }
-            else{
-                state.error = action.error.message;
-            }
-        })
-    }
-})
+  name: "user",
+  initialState: {
+    loading: false,
+    user: localUser ? localUser.users : null,
+    error: null,
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(loginUser.pending, (state) => {
+        state.loading = true;
+        state.user = null;
+        state.error = null;
+      })
+      .addCase(loginUser.fulfilled, (state, action) => {
+        state.loading = false;
+        state.user = action.payload;
+        state.error = null;
+      })
+      .addCase(loginUser.rejected, (state, action) => {
+        state.loading = false;
+        state.user = null;
+        console.log(action.error.message);
+        if (action.error.message === "Request failed with status code 401") {
+          state.error = "Access Denied! Invalid Credentials";
+        } else {
+          state.error = action.error.message;
+        }
+      });
+  },
+});
 
 export default userSlice.reducer;
