fdbf855aa5e3ebd3c1c53155d79c0d6917061064
kushkansagara <kush02032004@gmail.com>
2023-09-02 09:37:05 +0530

    change product style

--- SGP III/FrontEnd/devnarayan_site/src/Page/Products/Products.jsx
+++ SGP III/FrontEnd/devnarayan_site/src/Page/Products/Products.jsx
@@ -5,6 +5,9 @@ import './Products.scss'
 import ProductCard from '../../Components/Product/ProductCard';
 import '../../Components/Product/product.scss'
 import { Oval } from 'react-loader-spinner'
+import Navbar from '../../Components/Navbar/Navbar';
+import Footer from '../../Components/Footer/Footer'
+import Header from '../../Components/Header/Header';
 
 export default function Products() {
 
@@ -44,42 +47,58 @@ export default function Products() {
     })
 
     return (
-        <div className='products'>
-            <div className="filters">
-                <nav>
-                    <NavLink to={"/products"}>All</NavLink>
-                    <NavLink to={"/products?type=faluda"}> Faluda</NavLink>
-                    <NavLink to={"/products?type=lassi"}> Lassi</NavLink>
-                    <NavLink to={"/products?type=mojito"}> Mojito</NavLink>
-                    <NavLink to={"/products?type=thickshake"}> Thick Shake</NavLink>
-                    <NavLink to={"/products?type=shake"}> Shake</NavLink>
-                    <NavLink to={"/products?type=float"}> FLOAT</NavLink>
-                    <NavLink to={"/products?type=coffee"}> Coald Coffe</NavLink>
-                    <NavLink to={"/products?type=scoop"}> Scoop Ice Cream</NavLink>
-                    <NavLink to={"/products?type=seasonal"}> Seasonal Ice Cream</NavLink>
-                    <NavLink to={"/products?type=brownie"}> Brownie Ice Cream</NavLink>
-                </nav>
-            </div>
-            <div className="row owl-carousel product-carousel" >
-                {
-                    isLoading ?
-                        <div className="loading">
-                            <Oval
-                                height={80}
-                                width={80}
-                                color="#f94fee"
-                                wrapperStyle={{}}
-                                wrapperClass=""
-                                visible={true}
-                                ariaLabel='oval-loading'
-                                secondaryColor="#f94fee"
-                                strokeWidth={2}
-                                strokeWidthSecondary={2}
-                            />
+        <>
+            <Navbar />
+            <Header pageTitle="Products" breadcrumbs={['Home', 'Products']} />
+            <div className='aboutinfo'>
+                <div className="container-fluid py-5">
+                    <div className="container py-5">
+                        <div className="row justify-content-center">
+                            <div className="col-lg-7">
+                                <h1 className="section-title position-relative text-center mb-5">Best Prices We Offer For Ice Cream Lovers</h1>
+                            </div>
                         </div>
-                        : ShowProduct
-                }
+                    </div>
+                </div>
             </div>
-        </div>
+            <div className='products'>
+                <div className="filters">
+                    <nav>
+                        <NavLink to={"/products"}  >All</NavLink>
+                        <NavLink to={"/products?type=faluda"}> Faluda</NavLink>
+                        <NavLink to={"/products?type=lassi"} > Lassi</NavLink>
+                        <NavLink to={"/products?type=mojito"}> Mojito</NavLink>
+                        <NavLink to={"/products?type=thickshake"}> Thick Shake</NavLink>
+                        <NavLink to={"/products?type=shake"}> Shake</NavLink>
+                        <NavLink to={"/products?type=float"}> FLOAT</NavLink>
+                        <NavLink to={"/products?type=coffee"}> Coald Coffe</NavLink>
+                        <NavLink to={"/products?type=scoop"}> Scoop Ice Cream</NavLink>
+                        <NavLink to={"/products?type=seasonal"}> Seasonal Ice Cream</NavLink>
+                        <NavLink to={"/products?type=brownie"}> Brownie Ice Cream</NavLink>
+                    </nav>
+                </div>
+                <div className="row owl-carousel product-carousel" >
+                    {
+                        isLoading ?
+                            <div className="loading">
+                                <Oval
+                                    height={80}
+                                    width={80}
+                                    color="#f94fee"
+                                    wrapperStyle={{}}
+                                    wrapperClass=""
+                                    visible={true}
+                                    ariaLabel='oval-loading'
+                                    secondaryColor="#f94fee"
+                                    strokeWidth={2}
+                                    strokeWidthSecondary={2}
+                                />
+                            </div>
+                            : ShowProduct
+                    }
+                </div>
+            </div>
+            <Footer />
+        </>
     )
 }
